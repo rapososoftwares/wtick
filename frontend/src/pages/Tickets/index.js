3782cb3068a2db5cf2924c63e149130db2741abe
André Luis <andreluiz09444@gmail.com>
2024-03-21 08:52:47 -0300

    .

--- frontend/src/pages/Tickets/index.js
+++ frontend/src/pages/Tickets/index.js
@@ -7,7 +7,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import TicketsManager from "../../components/TicketsManager/";
 import Ticket from "../../components/Ticket/";
 
-import logo from "../../assets/logo.png"; //PLW DESIGN LOGO//
+import logo from "../../assets/logo.png";
 
 import { i18n } from "../../translate/i18n";
 
@@ -38,7 +38,7 @@ const useStyles = makeStyles(theme => ({
 		flexDirection: "column",
 	},
 	welcomeMsg: {
-		backgroundColor: theme.palette.boxticket, //DARK MODE PLW DESIGN//
+		backgroundColor: theme.palette.boxticket, 
 		display: "flex",
 		justifyContent: "space-evenly",
 		alignItems: "center",
@@ -65,11 +65,11 @@ const Chat = () => {
 							</>
 						) : (
 							<Paper square variant="outlined" className={classes.welcomeMsg}>
-							//PLW DESIGN LOGO//
+							
 							<div>
 							<center><img style={{ margin: "0 auto", width: "70%" }} src={logo} alt="logologin" /></center>
 							</div>
-							//PLW DESIGN LOGO//
+							
 							{/*<span>{i18n.t("chat.noTicketMessage")}</span>*/}
 							</Paper>
 						)}
